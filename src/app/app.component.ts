ceda03b122f4a63095e50553806d8df29934a335
Hiep0502 <dthiep.gm@gmail.com>
2019-05-24 02:34:17 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bai12-attributedirectives';
+
+  cone = true;
+  ctwo = true;
+
+  style = "italic";
+  size = '30px';
+
+
+  Toggle(){
+      this.cone = !this.cone;
+      this.ctwo = !this.ctwo;
+  }
 }
